ba650a9dbf6c016e08c86aeba00853b08948dc39
lgezelius <lgezelius@gmail.com>
2014-08-19 21:59:11 -0700

    completed assignment

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,70 @@
-run_analysis.R
+if (!file.exists("data")) {
+  dir.create("data")
+}
+download.file(
+  "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+  file.path("data","Dataset.zip"), 
+  method="curl"
+)
+unzip(file.path("data","Dataset.zip"),exdir="data")
+
+# function to convert a space delimited string of numbers to a numeric vector
+library(stringr)
+stringAsNV <- function(s) {
+  as.numeric(unlist(strsplit(gsub("  ", " ", str_trim(as.character(s))), split=" ")))
+}
+
+# read and combine the test and train data for subjects
+subject <- rbind(
+  read.csv(file.path("data","UCI HAR Dataset","test","subject_test.txt"), header=FALSE, col.names=("subject")),
+  read.csv(file.path("data","UCI HAR Dataset","train","subject_train.txt"), header=FALSE, col.names=("subject"))
+)
+
+# read the activity levels and labels from a space delimited file
+activity_labels <- read.csv(
+  file.path("data","UCI HAR Dataset","activity_labels.txt"),
+  header=FALSE, 
+  sep="",
+  col.names=c("level","label")
+)
+
+# read and combine the test and train data for activities
+y <- rbind(
+  read.csv(file.path("data","UCI HAR Dataset","test","y_test.txt"), header=FALSE, col.names=("activity")),
+  read.csv(file.path("data","UCI HAR Dataset","train","y_train.txt"), header=FALSE, col.names=("activity"))  
+)
+y$activity <- factor(y$activity, levels = activity_labels$level, labels = activity_labels$label)
+
+# read the column labels for the feature data
+columns <- read.csv(
+  file.path("data","UCI HAR Dataset","features.txt"),
+  header=FALSE,
+  sep="",
+  col.names=c("number","name")
+)
+
+# read and combine the test and train data for features
+X <- rbind(
+  read.csv(file.path("data","UCI HAR Dataset","test","X_test.txt"), header=FALSE),
+  read.csv(file.path("data","UCI HAR Dataset","train","X_train.txt"), header=FALSE)  
+)
+
+# create a character vector of column names containing "mean()" or "std()"
+mean_and_std_columns <- as.character(columns$name[grepl("(mean\\(\\)|std\\(\\))",columns$name)])
+
+# create the first tidy data set:
+# convert the strings of space delimited feature data to vectors
+t <- t(sapply(X[, 1], stringAsNV))
+# apply the labels for the feature columns
+colnames(t) <- columns$name
+# include only the mean() and std() feature data
+t <- t[, mean_and_std_columns]
+# join subject, activity, and feature data
+t <- cbind(subject, y, t)
+
+# create and write out the second tidy data set:
+library(plyr)
+# compute the mean of feature data, grouped by subject and activity
+t2 <- aggregate(t[, mean_and_std_columns], by=list(t$subject, t$activity), FUN=mean)
+t2 <- rename(t2, c("Group.1"="subject", "Group.2"="activity"))
+write.table(t2, file=file.path("data","aggregate.txt"), append=FALSE, sep=" ", row.names=FALSE)
